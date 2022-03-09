3bde93494f338821bcea6c45a3cd33202bbd5b2c
Gayathri K <95470777+rithvikteja@users.noreply.github.com>
2022-03-09 14:16:59 +0000

    gayathri

--- p1final.c
+++ p1final.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include<math.h>
 void input(int *num1,int *den1,int *num2,int *den2)
 {
   printf("enter the values of num1,den1,num2 and den2\n");
@@ -8,7 +7,7 @@ scanf("%d%d%d%d",num1,den1,num2,den2);
 void add(int num1,int den1,int num2,int den2,int *num3,int *den3)
 {
  int i,gcd;
-  *num3=(num1*den2)+(num2+den1);
+  *num3=(num1*den2)+(num2*den1);
   *den3=den1*den2;
   for(i=1;i<=*num3 && i<=*den3;i++)
     {
--- p2final.c
+++ p2final.c
@@ -1,1 +1,58 @@
 #include<stdio.h>
+struct _fraction
+{
+  int num,den;
+};
+typedef struct _fraction Fraction;
+Fraction input_fraction()
+{
+  Fraction f1;
+  printf("enter fraction in format a b\n");
+  scanf("%d %d",&f1.num,&f1.den);
+  return f1;
+}
+Fraction Smallest_fraction(Fraction f1,Fraction f2,Fraction f3)
+{
+  Fraction smallest;
+  if(f1.num*f2.den<f2.num*f1.den)
+  {
+    if(f1.num*f3.den<f3.num*f1.den)
+    {
+      smallest.num=f1.num;
+      smallest.den=f1.den;
+    }
+    else
+    {
+      smallest.num=f3.num;
+      smallest.den=f3.den;
+    } 
+  }
+  else
+  {
+    if(f2.num*f3.den<f3.num*f2.den)
+    {
+      smallest.num=f2.num;
+      smallest.den=f2.den;
+    }
+    else
+    {
+      smallest.num=f3.num;
+      smallest.den=f3.den;
+    }
+  }
+  return smallest;
+}
+void output(Fraction f1,Fraction f2,Fraction f3,Fraction smallest)
+{
+  printf("the greatest among %d/%d,%d/%d and %d/%d is %d/%d",f1.num,f1.den,f2.num,f2.den,f3.num,f3.den,smallest.num,smallest.den);
+}  
+int main()
+{
+  Fraction f1,f2,f3,smallest;
+  f1=input_fraction();
+  f2=input_fraction();
+  f3=input_fraction();
+  smallest=Smallest_fraction(f1,f2,f3);
+  output(f1,f2,f3,smallest);
+  return 0;
+}
