438702b4b64badd92eac93158d99811456a1901a
Gayathri K <95470777+rithvikteja@users.noreply.github.com>
2022-04-13 05:33:38 +0000

    modified

--- p2final.c
+++ p2final.c
@@ -6,46 +6,40 @@ struct _fraction
 typedef struct _fraction Fraction;
 Fraction input_fraction()
 {
-  Fraction f1;
-  printf("enter fraction in format a b\n");
-  scanf("%d %d",&f1.num,&f1.den);
-  return f1;
+   Fraction f;
+   printf("Enter the fraction:");
+  scanf("%d%d", &f.num,&f.den);
+  return f;
 }
-Fraction Smallest_fraction(Fraction f1,Fraction f2,Fraction f3)
+
+Fraction Smallest_fraction(Fraction f1,Fraction f2, Fraction f3)
 {
+  
   Fraction smallest;
-  if(f1.num*f2.den<f2.num*f1.den)
+  if(f1.num*f2.den< f2.num*f1.den) 
   {
-    if(f1.num*f3.den<f3.num*f1.den)
-    {
-      smallest.num=f1.num;
-      smallest.den=f1.den;
-    }
-    else
-    {
-      smallest.num=f3.num;
-      smallest.den=f3.den;
-    } 
+    if (f1.num*f3.den<f3.num*f1.den)
+    smallest.num=f1.num;
+    smallest.den=f1.den;
   }
-  else
+  else if (f2.num*f3.den<f3.num*f2.den)
   {
-    if(f2.num*f3.den<f3.num*f2.den)
-    {
-      smallest.num=f2.num;
-      smallest.den=f2.den;
-    }
-    else
-    {
-      smallest.num=f3.num;
-      smallest.den=f3.den;
-    }
+    smallest.num=f2.num;
+    smallest.den=f2.den;
+  }
+  else 
+  {
+    smallest.num=f3.num;
+    smallest.den=f3.den;
   }
   return smallest;
 }
-void output(Fraction f1,Fraction f2,Fraction f3,Fraction smallest)
+
+void output(Fraction f1, Fraction f2, Fraction f3, Fraction smallest)
 {
-  printf("the smallest among %d/%d,%d/%d and %d/%d is %d/%d",f1.num,f1.den,f2.num,f2.den,f3.num,f3.den,smallest.num,smallest.den);
-}  
+
+  printf("the smallest fraction is %d/%d", smallest.num,smallest.den);
+}
 int main()
 {
   Fraction f1,f2,f3,smallest;
